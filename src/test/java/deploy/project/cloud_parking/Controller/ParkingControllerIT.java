ad65d9345c49217001e2481da33ea91a6f8e92ef
João Antônio <joaocoelhoreis@gmail.com>
2025-05-30 14:08:47 -0300

    Adicionando a Depêndencia RestAssured e criando Teste de alguns Endpoints

---
+++ src/test/java/deploy/project/cloud_parking/Controller/ParkingControllerIT.java
@@ -0,0 +1,50 @@
+package deploy.project.cloud_parking.Controller;
+
+import deploy.project.cloud_parking.Controller.DTO.ParkingCreateDTO;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerIT {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest(){
+        System.out.println(randomPort);
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    void whenfindAllThemCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/parking")
+                .then()
+                .extract().response().body().prettyPrint();
+    }
+
+    @Test
+    void whenCreateParkingCheckResult() {
+        var createDTO = new ParkingCreateDTO();
+        createDTO.setColor("Prata");
+        createDTO.setLicense("JVC-3305");
+        createDTO.setModel("Gol");
+        createDTO.setState("GO");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/parking")
+                .then()
+                .statusCode(201)
+                .body("license", Matchers.equalTo("JVC-3305"));
+    }
+}
